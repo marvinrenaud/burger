f5243172e1738d9c1e34e096d56de53a4f613f61
M R <M R>
2017-05-06 09:53:16 -0400

    heroku fixes

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
 });
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
